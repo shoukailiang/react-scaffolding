0970749e256876a2e9dfd41285a1b0c27af9f54d
shoukailiang <shoukailiang@qq.com>
2018-05-25 11:24:54 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/stores';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
+
---
+++ src/redux/reducers.js
@@ -0,0 +1,9 @@
+//合并所有reducer 并返回
+import { combineReducers } from 'redux';
+// import * as ActionTypes from "./actionTypes"
+
+
+
+
+
+export default combineReducers({  });
